5b499c110e5976904fa2b9bcc03f88196bd47aa9
Özkan Pakdil <ozkan.pakdil@gmail.com>
2023-12-30 15:30:14 +0000

    https://stackoverflow.com/questions/77719096/get-directory-of-compiled-native-image-by-graalvm-at-runtime

---
+++ graalvm-exe-path-finder/README.md
@@ -0,0 +1,52 @@
+# example use of 
+
+<groupId>org.graalvm.nativeimage</groupId>
+<artifactId>library-support</artifactId>
+
+for https://stackoverflow.com/questions/77719096/get-directory-of-compiled-native-image-by-graalvm-at-runtime
+
+
+
+# My Gluon Application
+
+This Gluon sample was generated from https://start.gluon.io
+
+## Basic Requirements
+
+A list of the basic requirements can be found online in the [Gluon documentation](https://docs.gluonhq.com/#_requirements).
+
+## Quick instructions
+
+### Run the sample on JVM/HotSpot:
+
+    mvn gluonfx:run
+f
+### Run the sample as a native image:
+
+    mvn gluonfx:build gluonfx:nativerun
+
+### Run the sample as a native android image:
+
+    mvn -Pandroid gluonfx:build gluonfx:package gluonfx:install gluonfx:nativerun
+
+### Run the sample as a native iOS image:
+
+    mvn -Pios gluonfx:build gluonfx:package gluonfx:install gluonfx:nativerun
+
+## Selected features
+
+This is a list of all the features that were selected when creating the sample:
+
+### JavaFX 21 Modules
+
+ - javafx-base
+ - javafx-graphics
+ - javafx-controls
+
+### Gluon Features
+
+ - Glisten: build platform independent user interfaces
+ - Attach display
+ - Attach lifecycle
+ - Attach statusbar
+ - Attach storage
---
+++ graalvm-exe-path-finder/src/main/java/com/mycompany/sample/Main.java
@@ -0,0 +1,79 @@
+package com.mycompany.sample;
+
+import com.gluonhq.attach.display.DisplayService;
+import com.gluonhq.attach.util.Platform;
+import com.gluonhq.charm.glisten.application.AppManager;
+import com.gluonhq.charm.glisten.control.AppBar;
+import com.gluonhq.charm.glisten.control.FloatingActionButton;
+import com.gluonhq.charm.glisten.mvc.View;
+import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.application.Application;
+import javafx.geometry.Dimension2D;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.graalvm.nativeimage.ProcessProperties;
+
+import static com.gluonhq.charm.glisten.application.AppManager.HOME_VIEW;
+
+public class Main extends Application {
+
+    private final AppManager appManager = AppManager.initialize(this::postInit);
+
+    @Override
+    public void init() {
+        appManager.addViewFactory(HOME_VIEW, () -> {
+            FloatingActionButton fab = new FloatingActionButton(MaterialDesignIcon.SEARCH.text,
+                    e -> System.out.println("Search"));
+
+            ImageView imageView = new ImageView(new Image(Main.class.getResourceAsStream("openduke.png")));
+            imageView.setFitHeight(200);
+            imageView.setPreserveRatio(true);
+
+            Label label = new Label("Hello, My Gluon Application!");
+
+            VBox root = new VBox(20, imageView, label);
+            root.setAlignment(Pos.CENTER);
+
+            View view = new View(root) {
+                @Override
+                protected void updateAppBar(AppBar appBar) {
+                    appBar.setTitleText("My Gluon Application");
+                }
+            };
+
+            fab.showOn(view);
+
+            return view;
+        });
+    }
+
+    @Override
+    public void start(Stage stage) {
+        appManager.start(stage);
+    }
+
+    private void postInit(Scene scene) {
+        Swatch.LIGHT_GREEN.assignTo(scene);
+        scene.getStylesheets().add(Main.class.getResource("styles.css").toExternalForm());
+        ((Stage) scene.getWindow()).getIcons().add(new Image(Main.class.getResourceAsStream("/icon.png")));
+
+        if (Platform.isDesktop()) {
+            Dimension2D dimension2D = DisplayService.create()
+                    .map(DisplayService::getDefaultDimensions)
+                    .orElse(new Dimension2D(640, 480));
+            scene.getWindow().setWidth(dimension2D.getWidth());
+            scene.getWindow().setHeight(dimension2D.getHeight());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("exe path:"+ProcessProperties.getExecutableName());
+        launch(args);
+    }
+}
